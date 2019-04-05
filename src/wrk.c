eaefdc82be2ddebe59c0a17b2a8f8fe1fea990c8
Tom Barbette <barbette@kth.se>
2019-04-05 09:55:00 +0200

    Argument parsing fix

--- src/wrk.c
+++ src/wrk.c
@@ -532,7 +532,7 @@ static int parse_args(struct config *cfg, char **url, struct http_parser_url *pa
     cfg->raw         = 0;
     cfg->affinity    = -1;
 
-    while ((c = getopt_long(argc, argv, "t:c:d:s:H:b:T:Lrv?", longopts, NULL)) != -1) {
+    while ((c = getopt_long(argc, argv, "t:c:d:s:H:a:b:T:Lrv?", longopts, NULL)) != -1) {
         switch (c) {
             case 't':
                 if (scan_metric(optarg, &cfg->threads)) return -1;
